ecee10964bc8214fb8747c8d4d2453c42ae3bb50
= <damierny@gmail.com>
2019-05-23 22:52:37 -0500

    Removed something.

--- lua/entities/marums_hoverball/cl_init.lua
+++ lua/entities/marums_hoverball/cl_init.lua
@@ -2,6 +2,5 @@ include('shared.lua')
 
 function ENT:Draw()
     -- self.BaseClass.Draw(self) -- Overrides Draw
-    --self:DrawModel() -- Draws Model Client Side
-    self.Entity:DrawModel() -- PewpewSafezone does this, so im gonna take an example.
+    self:DrawModel() -- Draws Model Client Side
 end
