6cd520d96c70e63e797c3c2f9aadead2b1aef049
= <damierny@gmail.com>
2019-05-23 23:09:48 -0500

    Fix overindentation

--- lua/weapons/gmod_tool/stools/hoverball_spawner.lua
+++ lua/weapons/gmod_tool/stools/hoverball_spawner.lua
@@ -29,7 +29,7 @@ function TOOL:LeftClick( trace )
         if (IsValid(trace.Entity)) then
             local weld = constraint.Weld( entity, trace.Entity, 0, trace.PhysicsBone, 0, true , false )
         end
-            self:GetOwner():AddCleanup( "Marums Hovers", entity )
+        self:GetOwner():AddCleanup( "Marums Hovers", entity )
         undo.Create( "Marums HoverBall" )
             undo.AddEntity( entity )
             undo.SetPlayer( self:GetOwner() )
